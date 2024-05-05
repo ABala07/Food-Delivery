cdc72e886a87fc811870063f937b3f55a0592b8b
abala <areldi.bala@n-fuse.co>
2024-05-05 04:10:23 +0300

    Create the Header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Choose from a great menu featuring a delectable array of dishes
+          crafted with the flavor of many amazing foods
+        </p>
+        <button className="">View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Home.css';
+import Header from '../../components/Header/Header';
 
 function Home() {
-  return <div></div>;
+  return(
+   <div>
+    <Header/>
+  </div>
+  
+  )
 }
 
 export default Home;
